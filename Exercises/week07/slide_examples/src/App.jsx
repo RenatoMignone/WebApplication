a7147488cc4c2a1145a965eafda249d35603540c
renato <renato.mignone@gmail.com>
2025-04-08 16:00:31 +0200

    ex mod

---
+++ Exercises/week07/slide_examples/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import MyButton from './Button.jsx'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+	return (
+		<p>
+		Press here: <MyButton lang='it' />
+		</p>
+	);
+}
+
+export default App
